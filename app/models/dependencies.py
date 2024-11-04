87d778a9cebaf786d933b36f74a94ff9c5cc6882
mohammedarbaz119 <mohammedarbazq16@gmail.com>
2024-11-05 03:31:02 +0530

    fixed db not created issue

--- app/models/dependencies.py
+++ app/models/dependencies.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel,Session,create_engine
 from fastapi import Depends
 from typing import Annotated
-
+import os
 
 sqlite_file_name = "data/database/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -10,6 +10,7 @@ connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def create_db_and_tables():
+    os.makedirs(os.path.dirname(sqlite_file_name), exist_ok=True)
     SQLModel.metadata.create_all(engine)
 
 
